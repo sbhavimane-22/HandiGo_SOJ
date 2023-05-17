cad8442fd9997bcd84abbbc1aec81b8f179108f6
sbhavimane-22 <sbhavimane@uchicago.edu>
2023-05-17 16:58:54 -0500

    newHome

--- Backend/Sources/App/routes.swift
+++ Backend/Sources/App/routes.swift
@@ -126,7 +126,7 @@ extension Ride: Content {}
 extension Request {
     /// Convenience extension for obtaining a collection.
     var rideCollection: MongoCollection<Ride> {
-        self.application.mongoDB.client.db("home").collection("rides", withType: Ride.self)
+        self.application.mongoDB.client.db("newHome").collection("rides", withType: Ride.self)
     }
 
     func findRides() async throws -> [Ride] {
